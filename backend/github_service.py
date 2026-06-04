fb9833d3156747b6b132c2d1f12695987cfc5759
akshadhas01 <akshadhas01@gmail.com>
2026-06-05 00:43:18 +0530

    Add README extraction endpoint

--- backend/github_service.py
+++ backend/github_service.py
@@ -1,8 +1,28 @@
 import requests
+import base64
+
 
 def get_repo(owner, repo):
     url = f"https://api.github.com/repos/{owner}/{repo}"
+    response = requests.get(url)
+    return response.json()
+
+
+def get_languages(owner, repo):
+    url = f"https://api.github.com/repos/{owner}/{repo}/languages"
+    response = requests.get(url)
+    return response.json()
+
+
+def get_readme(owner, repo):
+    url = f"https://api.github.com/repos/{owner}/{repo}/readme"
 
     response = requests.get(url)
 
-    return response.json()
+    data = response.json()
+
+    content = base64.b64decode(
+        data["content"]
+    ).decode("utf-8")
+
+    return content[:5000]
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
-from github_service import get_repo
+from github_service import (
+    get_repo,
+    get_languages,
+    get_readme
+)
 
 app = FastAPI()
+
 
 @app.get("/")
 def home():
@@ -9,6 +14,7 @@ def home():
         "project": "Software Archaeologist",
         "status": "running"
     }
+
 
 @app.get("/repo/{owner}/{repo}")
 def repo_info(owner: str, repo: str):
@@ -20,4 +26,16 @@ def repo_info(owner: str, repo: str):
         "description": data["description"],
         "stars": data["stargazers_count"],
         "language": data["language"]
+    }
+
+
+@app.get("/languages/{owner}/{repo}")
+def languages(owner: str, repo: str):
+    return get_languages(owner, repo)
+
+
+@app.get("/readme/{owner}/{repo}")
+def readme(owner: str, repo: str):
+    return {
+        "readme": get_readme(owner, repo)
     }
