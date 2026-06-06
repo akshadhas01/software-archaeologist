fd8f0d58b630754bd6c0eb81d1e76ac69efe6c29
akshadhas01 <akshadhas01@gmail.com>
2026-06-06 23:29:53 +0530

    Connect React dashboard to FastAPI backend

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from services.github_service import (
     get_repo,
     get_languages,
@@ -9,6 +11,16 @@ from services.github_service import (
 )
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
